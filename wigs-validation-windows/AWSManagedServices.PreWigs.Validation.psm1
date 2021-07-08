db5221a9756daf3265e047cdbc8e5444d6181830
Nitin Verma <vernitin@amazon.com>
2021-07-09 07:38:16 +1000

    Removal of unwanted code

--- wigs-validation-windows/AWSManagedServices.PreWigs.Validation.psm1
+++
@@ -1,7 +0,0 @@
-﻿# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-Get-ChildItem -Path $PSScriptRoot -Recurse -Include *.ps1 -Exclude *.Tests.ps1 | ForEach-Object {
-    . $_.FullName
-    Export-ModuleMember -Function $_.BaseName -Alias *
-}
